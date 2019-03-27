16d70228a79465e38ba556b782f5c5b9111bdf57
mastergostercreation <master.goster.creation@gmail.com>
2019-03-27 10:39:58 +0000

    etape 3 : augmenter le score

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 var button = document.getElementById("clic");
 var div = document.getElementById("affichage") ;
 var score = 0;
+
+
+function clic() { 
+	score++;
+	//score+1
+	div.innerHTML = score;
+};
