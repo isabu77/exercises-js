3a5ab222b278dcbe1b96488d01d6490dc840417e
mastergostercreation <master.goster.creation@gmail.com>
2019-03-27 11:24:17 +0000

    etape 7 : on ne fait pas credit

--- script.js
+++ script.js
@@ -11,13 +11,16 @@ function clic() {
 };
 
 function augmenterMultiplicateur() {
-	//retire 50 au score
-	score -= 50;
-	//affiche le score dans la div
-	div.innerHTML = score;
-	// ajoute +1 au multiplicateur
-	multiplicateur++;
-	//affiche la valeur multiplicateur sur le bouton
-	buttonMultiplier.innerHTML = multiplicateur;
+	if(score >= 50){
+		//retire 50 au score
+		score -= 50;
+		//affiche le score dans la div
+		div.innerHTML = score;
+		// ajoute +1 au multiplicateur
+		multiplicateur++;
+		//affiche la valeur multiplicateur sur le bouton
+		buttonMultiplier.innerHTML = multiplicateur;
+	}
+	
 	
 }
