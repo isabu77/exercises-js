c8cc3cec1ac0f72ddac88af4e6892de819869df2
mastergostercreation <master.goster.creation@gmail.com>
2019-03-27 10:50:33 +0000

    etape 4 : preparation du multiplicateur

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 var button = document.getElementById("clic");
+var buttonMultiplier = document.getElementById("multiplier");
 var div = document.getElementById("affichage") ;
 var score = 0;
-
+var multiplicateur =1;
 
 function clic() { 
 	score++;
 	//score+1
 	div.innerHTML = score;
 };
+
+function augmenterMultiplicateur() {
+	multiplicateur++;
+	buttonMultiplier.innerHTML = multiplicateur;
+	
+}
