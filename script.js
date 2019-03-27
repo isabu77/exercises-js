02049fbd800e755f3ac928ea5b9ea10575cf1172
mastergostercreation <master.goster.creation@gmail.com>
2019-03-27 13:37:19 +0000

    etape 11 : autoclicker

--- script.js
+++ script.js
@@ -4,11 +4,23 @@ var div = document.getElementById("affichage") ;
 var score = 0;
 var multiplicateur =1
 var prixMultiplicateur= 50;
+//autoclick arreter 0 sinon 1
+var autoclick = 0;
+
 
 function clic() { 
 	score += multiplicateur;
 	//score+multiplicateur
 	div.innerHTML = score;
+	//score supérieur à 200
+	if (score >= 200){
+		//si pas deja d'autoclic
+		if(autoclick === 0){
+			autoclick = 1;
+			intervalID = setInterval(clic, 1000);
+		}
+	}
+
 };
 
 function augmenterMultiplicateur() {
@@ -25,6 +37,9 @@ function augmenterMultiplicateur() {
 		buttonMultiplier.innerHTML =  "Multiplicateur x" + multiplicateur + " (-"+ prixMultiplicateur +" clics)";
 		
 	}
-	
-	
 }
+
+
+
+
+
